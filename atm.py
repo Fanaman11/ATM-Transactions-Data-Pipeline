b903fed336635f84f7b62521c880152b204370ab
Fanaman11 <faithanaman@gmail.com>
2026-03-25 11:23:13 -0400

    Implement basic ATM functionality
    Fix balance handling and improve menu options.

---
+++ atm.py
@@ -0,0 +1,30 @@
+balance=0
+while True:
+    print("---ATM MENU---")
+    print("1. Check Balnce")
+    print("2.Deposit")
+    print("3.Withdraw")
+    print("4.Exit")
+
+    choice = input("choose an option:")
+    if choice =="1":
+        print("Your balance is:",balance)
+    elif choice =="2":
+        amount= float(input("Enter amount to deposit: "))
+        balance += deposit
+        print ("Deposited:", amount)
+    elif choice == "3":
+        amount=float(input("Enter amount to withdraw:"))
+        if amount > balance:
+            print ("Insufficient funds!")
+        else:
+            balance-= amount
+            print("Withdrawn:, amount")
+    elif choice =="4":
+        print("Thank you, have a nice day!")
+        break
+    else:
+      print("Invalid option. Try again.")
+
+
+    
